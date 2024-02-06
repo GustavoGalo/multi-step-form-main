7249d7809693b9ad110018bcd6299f37a755a5c0
Gustavo Galo <gg.gustavo.galo@gmail.com>
2024-02-05 21:51:23 -0300

    feat: add ubuntu font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          marine: 'hsl(var(--marine-blue) / <alpha-value>)',
+          purplish: 'hsl(var(--purplish-blue) / <alpha-value>)',
+          pastel: 'hsl(var(--pastel-blue) / <alpha-value>)',
+          light: 'hsl(var(--light-blue) / <alpha-value>)',
+        },
+        red: {
+          strawberry: 'hsl(var(--strawberry-red) / <alpha-value>)',
+        },
+        gray: {
+          cool: 'hsl(var(--cool-gray) / <alpha-value>)',
+          light: 'hsl(var(--light-gray) / <alpha-value>)',
+        },
+        magnolia: 'hsl(var(--magnolia) / <alpha-value>)',
+        alabaster: 'hsl(var(--alabaster) / <alpha-value>)',
+        white: 'hsl(var(--white) / <alpha-value>)',
+      },
+      fontFamily: {
+        'ubuntu': ['Ubuntu']
+      }
+    },
   },
   plugins: [],
 }
